08f1272839a50db6a193b0d61288cdfd78807974
oliviarodrigos <isabela.coutinho.brito@escola.pr.gov.br>
2026-06-11 11:21:53 +0000

    isa

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
@@ -10,4 +11,31 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
   };
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2027-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2027-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2027-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2027-02-01T00:00:00");
+let tempoAtual = new Date();
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
 }
